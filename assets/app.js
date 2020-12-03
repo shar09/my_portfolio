1af4409a3ce9aa3222ef58370009ab3afcc1daa9
shar09 <sharathtelu9@gmail.com>
2020-12-03 09:33:06 -0600

    updated styles

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,15 @@ const displayNav = () => {
     const burger = document.querySelector('#menu-bars');
     const nav = document.querySelector('.links');
     const project = document.querySelector('#project-link');
-    const resume = document.querySelector('#resume-link');
-    const photos = document.querySelector('#photos-link');
+    // const resume = document.querySelector('#resume-link');
+    // const photos = document.querySelector('#photos-link');
     const toggleMove = document.querySelector('.page-move');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         project.classList.toggle('border-bottom');
-        resume.classList.toggle('border-bottom');
-        photos.classList.toggle('border-bottom');
+        // resume.classList.toggle('border-bottom');
+        // photos.classList.toggle('border-bottom');
         toggleMove.classList.toggle('toggle-page-move');
     });
 }
